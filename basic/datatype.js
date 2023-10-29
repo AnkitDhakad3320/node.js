5fd3a4d8f46778bcf4ab5f7f31870abf2449163f
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-10-30 03:26:55 +0530

    firdt red

---
+++ basic/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // terat all js code as newer version 
+// alert ( 3+3 );
+console.log( 3+3 )
+
+console.log("Ankit")
+
+let Name = "Ankit Dhakad"
+let age = 26
+let isLoggedIn = false
+let state;
+
+console.log([Name, age, isLoggedIn, state]);
+console.log(typeof"Ankit Dhakad")
+console.log(typeof 26)
+console.log(typeof undefined)
+console.log(typeof null)
