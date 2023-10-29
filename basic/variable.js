f24919b2b74194e6406b03eb5948855c1f81ef76
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-10-30 02:17:11 +0530

    first commit

--- basic/variable.js
+++ basic/variable.js
@@ -1,15 +1,15 @@
 // console.log("Abhi");
 const accountId = 123554
-// let accountEmail = "ankitdhakad332@gmail.com"
-// var accountpassword = "135246"
-// accountCity = "delhi"
-// let accountState;
+let accountEmail = "ankitdhakad332@gmail.com"
+var accountpassword = "135246"
+accountCity = "delhi"
+var accountState;
 
 // accountId = 225142
 accountEmail = "ankit@4526.gmail.com"
 accountCity = "bhopal"
-accountpassword = "1253625"
-accountState;
+var accountpassword = "1253625"
+accountState = "m.p."
 
 console.log(accountId);
-console.table([accountEmail, accountpassword, accountCity, accountState])
+console.table([accountEmail, accountpassword, accountCity,accountState])
