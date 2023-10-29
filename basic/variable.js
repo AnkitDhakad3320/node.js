935ecf19c0cf6402b05d1e6aecf35fdaeddd2c65
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-10-30 01:42:47 +0530

    modified

--- basic/variable.js
+++ basic/variable.js
@@ -1,1 +1,15 @@
-console.log("Ankit");
+// console.log("Abhi");
+const accountId = 123554
+// let accountEmail = "ankitdhakad332@gmail.com"
+// var accountpassword = "135246"
+// accountCity = "delhi"
+// let accountState;
+
+// accountId = 225142
+accountEmail = "ankit@4526.gmail.com"
+accountCity = "bhopal"
+accountpassword = "1253625"
+accountState;
+
+console.log(accountId);
+console.table([accountEmail, accountpassword, accountCity, accountState])
