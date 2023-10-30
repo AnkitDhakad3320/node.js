a81f7037a3d28770b82fe2f7f3540babf570a227
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-10-31 00:51:23 +0530

    first msg

---
+++ basic/conservation-operation.js
@@ -0,0 +1,68 @@
+let score = "Ankit"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// let valueInNumber = Number(33)
+// let valueInNumber = Number(33abc)
+let valueInNumber = 33
+
+
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+let isLoggedIn = "Ankit"
+// console.log(typeof isLoggedIn)
+// console.log(isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(+booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(5%3);
+
+
+let str1 = "hello"
+let str2 = "Ankit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// console.log(true)
+// console.log(false)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
