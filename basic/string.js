202d0c07bfabe08361c86737b6a12e8c2bd52f53
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-10-31 04:34:46 +0530

    fisrt trial

---
+++ basic/string.js
@@ -0,0 +1,35 @@
+const name = "Ankit"
+const repocount = 50
+
+// console.log(name + repocount + "value" )
+console.log(` My Name is ${name} and My repocount is ${repocount}`)
+
+const gameName = new String ( 'Ankit-hc-dk')
+// console.log(gameName[1],gameName[9])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// console.log(gameName.indexOf(+'t'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anotherString = gameName.slice(-9,5);
+// console.log(anotherString);
+
+const newStringOne = "    Ankit    " 
+// console.log(newStringOne);
+// console.log(newStringOne.trim())
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sunder'))
+
+console.log(gameName.split('-'));
