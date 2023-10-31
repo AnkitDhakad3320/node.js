7ddd4ac57a9d9c844706cb3a447dfc94f71f94ea
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-10-31 23:49:17 +0530

    last

--- basic/num-and-maths.js
+++ basic/num-and-maths.js
@@ -1,15 +1,26 @@
 const score = 500
-console.log(score);
+// console.log(score);
 
 const balance = new Number(200.49)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(1))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
 
 const otherNumber = 23.378
-console.log(otherNumber.toPrecision(3))
-console.log(otherNumber.toString())
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toString())
 
 const hundreds = 20000000
-console.log(hundreds.toLocaleString('en-In'));
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+// ==========
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(6.8))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(6.9))
+console.log(Math.min( 8 , 5 , 6 , 9 , 2 ))
+console.log(Math.max( 2 , 9 , 7 , 4 ))
