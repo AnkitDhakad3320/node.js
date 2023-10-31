aceeb329e651249642300f614ef9c7d0457f8903
AnkitDhakad3320 <ankitdhakad3320@gmail.com>
2023-11-01 00:18:57 +0530

    last end

--- basic/num-and-maths.js
+++ basic/num-and-maths.js
@@ -17,10 +17,19 @@ const hundreds = 20000000
 
 // ==========
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(6.8))
-console.log(Math.ceil(4.1))
-console.log(Math.floor(6.9))
-console.log(Math.min( 8 , 5 , 6 , 9 , 2 ))
-console.log(Math.max( 2 , 9 , 7 , 4 ))
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.8))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(6.9))
+// console.log(Math.min( 8 , 5 , 6 , 9 , 2 ))
+// console.log(Math.max( 2 , 9 , 7 , 4 ))
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+// console.log(Math.floor(Math.random()*10 + 1 ));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1 )) + min )
